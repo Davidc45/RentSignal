5adf50cce8380090ed74e09e368a8814961e010a
Diego <diegosalas_2001@hotmail.com>
2026-03-17 18:31:08 -0700

    Configure Redux Toolkit store skeleton

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import './globals.css';
+import { Providers } from '../lib/providers';
 
 export const metadata = {
   title: 'RentSignal',
@@ -11,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body>
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
